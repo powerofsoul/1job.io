5f5071afb7b8688670b357ebebbe68452f8bb9ae
Florin Munteanu <florinmunteanu96@gmail.com>
2020-04-25 17:14:51 +0300

    Add footer and header

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -1,1 +1,8 @@
-export default () => <div>Hi we are the best</div>
+import Header from "../src/common/Header";
+import Footer from "../src/common/Footer";
+
+export default () => <div>
+    <Header />
+    Hi we are the best
+    <Footer />
+</div>
