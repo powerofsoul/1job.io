6bc4c34c11d01252df0de182523f4101535f1b51
Silviu <silviu@astorian.com>
2020-04-25 18:46:45 +0300

    Add first pallete colors

--- src/style/Colors.ts
+++ src/style/Colors.ts
@@ -1,11 +1,15 @@
-type Colors = {
-    [key: string]: string
-}
-
 const colors =  {
-    red: "red",
-    black: "black",
-    white: "white"
-} 
+    dark: "#1e1f26",
+    dark_dark: "#1a1b1f",
+    dark_light: "#333642",
+    dark_accent: "#666b7a",
+    primary: "#4077D0",
+    primary_dark: "#24406f",
+    primary_light: "#4d8cf2",
+    primary_accent: "#76a1c6",
+    green: "#5caf75",
+    red: "#f44336",
+    orange: "#f44336",
+} as const;
 
 export default colors;
