994c5a5ded5e20248f91fbec9fcb38efc6370e04
Florin Munteanu <florinmunteanu96@gmail.com>
2020-04-25 21:33:28 +0300

    Change header

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -3,29 +3,24 @@ import Colors from "../style/Colors";
 import { Row, Col, Space } from "antd";
 import Typist from "react-typist";
 import TypistLoop from 'react-typist-loop'
+import DeviceSize from "../style/DeviceSize";
 
 const Header = styled.div`
     width: 100%;
     background-color: ${Colors.light};
-`;
 
-const Title = styled.h1`
-    display: flex;
+    ${DeviceSize.md} {
+        padding: 20px;
+    }
 `;
 
 export default () => {
     return <Header>
         <Row>
             <Col xs={12}>
-                <Title>
-                    <TypistLoop interval={3000}>
-                        {[
-                            'Remote jobs for everyone',
-                            'Updated daily.',
-                            'Boost your career now!',
-                        ].map(text => <Typist key={text} startDelay={1000}>{text}</Typist>)}
-                    </TypistLoop>
-                </Title>
+                <h1>
+                   Jobs Remotely Online
+                </h1>
             </Col>
         </Row>
     </Header>
