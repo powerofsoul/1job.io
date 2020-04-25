dd104a58afff3d53ab8e3078627dbe445827bc19
Florin Munteanu <florinmunteanu96@gmail.com>
2020-04-25 19:32:00 +0300

    Add next css module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import 'antd/dist/antd.css';
+import styled from 'styled-components';
+import colors from '../src/style/Colors';
+
+const Body = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  background:
+		linear-gradient(90deg, ${colors.dark} ($dot-space - $dot-size), transparent 1%) center,
+		linear-gradient($bg-color ($dot-space - $dot-size), transparent 1%) center,
+		$dot-color;
+	background-size: $dot-space $dot-space;
+`;
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Body>
+    <Component {...pageProps} />
+  </Body>
 }
