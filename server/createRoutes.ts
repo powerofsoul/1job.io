acf919781b3d739d643d1ad966572fb436eda48c
Florin Munteanu <florinmunteanu96@gmail.com>
2020-04-27 00:06:38 +0300

    Fix prod require

--- server/createRoutes.ts
+++ server/createRoutes.ts
@@ -2,18 +2,18 @@ const glob = require('glob');
 import { Router } from "express";
 const router = Router();
 
- glob.sync(__dirname + '/api/**/*.ts')
-        .map(filename => {
-            const arr = filename.split('/')
-            let name = arr.pop();
-            name = name.replace('.ts', '')
-            return {
-                path: `/${name.toLowerCase()}`,
-                router: require(`${filename.replace('.ts', '')}`)
-            }
-        })
-        .forEach(r => {
-            router.use(r.path, r.router.default)
-        });
+glob.sync(__dirname + '/api/**/*.@(ts|js)')
+    .map(filename => {
+        const arr = filename.split('/')
+        let name = arr.pop();
+        name = name.replace('.ts', '').replace('.js', '')
+        return {
+            path: `/${name.toLowerCase()}`,
+            router: require(`${filename.replace('.ts', '').replace('js', '')}`)
+        }
+    })
+    .forEach(r => {
+        router.use(r.path, r.router.default)
+    });
 
 export default router;
