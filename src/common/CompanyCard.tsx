e658eef7414d33bd5b7610299275d60a4f9e0b6b
Florin Munteanu <florinmunteanu96@gmail.com>
2020-04-30 17:18:33 +0300

    Style job page

---
+++ src/common/CompanyCard.tsx
@@ -0,0 +1,36 @@
+import { User } from "models/User";
+import React from 'react';
+import { Avatar, Skeleton, Row, Col, Button } from "antd";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const CompanyCard = styled(Skeleton)`
+
+`;
+
+export default (props: { user?: User }) => {
+    const { user } = props;
+
+    return <CompanyCard avatar active loading={user == undefined}>
+        <Link to={`/profile/${user?._id}`}>
+            <Row align="middle" gutter={[12, 12]}>
+                <Col>
+                    <Avatar size={64} className="companyLogo" src={user?.companyImage} />
+                </Col>
+                <Col>
+                    {user?.companyName}
+                </Col>
+            </Row>
+        </Link>
+        {user?.companyWebsite && <Row>
+            <Col>
+                <a target="_blank" href={user.companyWebsite} className="ant-btn">
+                    View Website
+                </a>
+            </Col>
+        </Row>}
+        <Row>
+            {user?.companySummary}
+        </Row>
+    </CompanyCard>
+}
--- src/pages/job.tsx
+++ src/pages/job.tsx
@@ -1,53 +1,51 @@
 import { IAppState } from '../redux/configureStore';
 import { Job } from '../../models/Job';
 import { connect } from 'react-redux';
-import { Tag, Row, Col, Skeleton } from "antd";
+import { Tag, Row, Col, Skeleton, Avatar } from "antd";
 import colors from '../style/Colors';
 import styled from 'styled-components';
 import { get } from '../Utils';
 import { useState, useEffect } from 'react';
 import moment from 'moment';
 import React from "react";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useHistory } from 'react-router';
+import CompanyCard from '../common/CompanyCard';
+
 const JobDetails = styled.div`
+    padding-top: 5rem;
+    padding-bottom: 5rem;
+    
     .JobDetailsHeader {
         align-items:center;
 
         .CompanyDetails {
             display: block;
             align-items:center;
-
-            .companyLogo {
-                float: left;
-                margin-top: 1rem;
-                margin-right: 1rem;
-                min-height: 5rem;
-                max-height: 5rem;
-                min-width: 5rem;
-                max-width: 5rem;
-                border-radius: 50%;
-            }
         }
     }
     .JobDetailsBody {
         align-items:center;
         margin-top: 2rem;
     }
+
+    .no-padding {
+        padding: 0;
+    }
 `;
-  
+
 export default () => {
     const { id } = useParams();
     const [job, setJob] = useState<Job>()
     const [loading, setLoading] = useState(true)
     const history = useHistory();
-    
-    const fetch =  () => {
+
+    const fetch = () => {
         get(`/api/job/${id}`).then((j: Job) => {
             setJob(j);
             setLoading(false);
         }).catch(() => {
-            history.push("/"); 
+            history.push("/");
         });
     }
 
@@ -60,30 +58,23 @@ export default () => {
     return <JobDetails>
         <Skeleton active loading={loading}>
             <Row justify="center">
-                <Col xs={24} md={7}>
-                    <div className="JobDetailsHeader">
-                        <div>
-                            {moment(job?.postedOn).fromNow()}
-                        </div>
-                        <h1>
-                            {job?.title}
-                        </h1>
-                        <div className="CompanyDetails">
-                            <img className="companyLogo" src={job?.company.companyImage} />
-                            <a href={job?.company.companyWebsite} target="_blank">
-                                <h3>
-                                    {job?.company.companyName}
-                                </h3>
-                            </a>
-                            <div dangerouslySetInnerHTML={{__html: job?.company.companyDescription}} />
-                        </div>
+                <Col xs={12} md={9}>
+                    <div>
+                        Posted {moment(job?.postedOn).fromNow()}
+                    </div>
+                    <h1>
+                        {job?.title}
+                    </h1>
+                    <div className="JobDetailsBody">
+                        <div className="ql-editor no-padding" dangerouslySetInnerHTML={{ __html: job?.description }} />
                     </div>
                 </Col>
-            </Row>
-            <Row justify="center">
-                <Col xs={24} md={7}>
-                    <div className="JobDetailsBody">
-                        <div className="ql-editor" dangerouslySetInnerHTML={{ __html: job?.description }} />
+                <Col xs={12} md={3}>
+                    <div className="JobDetailsHeader">
+                        <div className="CompanyDetails">
+                            <CompanyCard user={job?.company} />
+                            <div dangerouslySetInnerHTML={{ __html: job?.company.companyDescription }} />
+                        </div>
                     </div>
                 </Col>
             </Row>
