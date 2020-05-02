edec6cea30b30f73750aa2354347c6fb361e248b
Florin Munteanu <florinmunteanu96@gmail.com>
2020-05-02 10:52:23 +0300

    Check errors in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+check_last_error() {
+    if [ "$1" != "0" ]; then
+        echo $2
+        popd
+        exit 127
+    fi
+}
 
 AWS_S3_REGION="us-east-1"
 PRODUCTION_BRANCH="master"
@@ -26,14 +33,18 @@ aws cloudfront create-invalidation \
   --distribution-id $CLOUDFRONT_DIST_ID \
   --paths /\*
 
+check_last_error $? "Something went wrong deploying to cloudfront"
+
 mkdir -p ~/.aws
 echo '[profile eb-cli]' > ~/.aws/config
 echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/config
 echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
 eb status
+check_last_error $? "Something went wrong authenticating to elastic beanstalk"
 
 EB_ENV="$EB_APP"
 echo "Deploying to $EB_ENV"
 eb deploy $EB_ENV -v
+check_last_error $? "Something went wrong deploying to elastic beanstalk"
 
 exit 0
