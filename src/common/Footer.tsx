f4abeb6186a259c9696e9fac447e3885097836b7
Florin Munteanu <florinmunteanu96@gmail.com>
2020-05-14 13:55:59 +0300

    Add instagram icon

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import DeviceSize from "../style/DeviceSize";
 import colors from "../style/Colors";
 import React from "react";
-import { FacebookFilled, LinkedinFilled, TwitterSquareFilled } from "@ant-design/icons";
+import { FacebookFilled, LinkedinFilled, TwitterSquareFilled, InstagramFilled } from "@ant-design/icons";
 
 const {Footer} = Layout;
 const StyledFooter = styled(Footer)`
@@ -48,9 +48,10 @@ export default () => {
                         <Link className="link" to="/blog">Blog</Link>
                         <Link className="link" to="/tos">Terms of Service</Link>
                         <Link className="link" to="/privacy">Privacy Policy</Link>
-                        <a target="_blank" className="link" href="https://twitter.com/1JOB9"><TwitterSquareFilled style={{fontSize: "2rem"}} /></a>
-                        <a target="_blank" className="link" href="https://www.linkedin.com/company/1jobdotio"><LinkedinFilled style={{fontSize: "2rem"}} /></a>
-                        <a target="_blank" className="link" href="https://www.facebook.com/1JOB-100100101713880"><FacebookFilled style={{fontSize: "2rem"}}/></a>
+                        <a target="_blank" className="link" href="https://www.instagram.com/1job.io/"><InstagramFilled style={{fontSize: "1.5rem"}} /></a>
+                        <a target="_blank" className="link" href="https://twitter.com/1JOB9"><TwitterSquareFilled style={{fontSize: "1.5rem"}} /></a>
+                        <a target="_blank" className="link" href="https://www.linkedin.com/company/1jobdotio"><LinkedinFilled style={{fontSize: "1.5rem"}} /></a>
+                        <a target="_blank" className="link" href="https://www.facebook.com/1JOB-100100101713880"><FacebookFilled style={{fontSize: "1.5rem"}}/></a>
                     </div>
                  </FooterDetails>
             </Col>
